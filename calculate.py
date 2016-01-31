ae28623d9ed52e044bc8caa47cb9c8c950d6122f
bradsk88 <7kcwKg60tdcW>
2016-01-31 00:07:19 -0600

    WIP

--- calculate.py
+++ calculate.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from math import sin, radians, cos, sqrt, asin
 
 import ijson
@@ -9,10 +10,17 @@ def run():
     json_file = open('/home/bradsk88/Desktop/finance/LocationHistory.json')
     locations = ijson.parse(json_file)
 
-    prev_lat = None
-    prev_lon = None
+    prev_lat = 0
+    prev_lon = 0
+    prev_appended_lat = 0
+    prev_appended_lon = 0
+    timestamp = None
 
     total_kms = 0
+
+    date_name = None
+
+    events = []
 
     cont = True
     while (cont):
@@ -22,17 +30,56 @@ def run():
 
         cont = True
 
+        if prefix == 'locations.item.timestampMs':
+            i_date = datetime.datetime.fromtimestamp(int(value)/1000)
+            timestamp = value
+
+            if i_date.date() != date_name:
+                # Date has changed
+                if date_name is not None:
+                    with open("days/{}.json".format(date_name.strftime("%Y-%m-%d")), 'w') as outfile:
+                        json.dump(list(reversed(events)), outfile, indent=3)
+                        print("Wrote to {}".format(outfile.name))
+                        events = []
+                    date_name = i_date.date()
+                else:
+                    date_name = i_date.date()
+            continue
+
+
         if (prefix, event) == ('locations.item.latitudeE7', 'number'):
             lat = value / 10000000
+            lat_e7 = value
             next(locations)
             prefix, event, value = next(locations)
             if (prefix, event) == ('locations.item.longitudeE7', 'number'):
                 lon = value / 10000000
+                lon_e7 = value
 
                 if prev_lat and prev_lon:
                     distance = haversine(prev_lat, prev_lon, lat, lon)
                     total_kms += distance
-                    print("Distance From {}, {} to {}, {}: {}km [Total: {}km]".format(prev_lat, prev_lon, lat, lon, distance, total_kms))
+                    # print("Distance From {}, {} to {}, {}: {}km [Total: {}km]".format(prev_lat, prev_lon, lat, lon, distance, total_kms))
+
+                difference_lat = abs(lat - prev_appended_lat)
+                difference_lon = abs(lon - prev_appended_lon)
+
+                if max(difference_lat, difference_lon) > 0.0040:
+
+                    events.append({
+                        'timestampMs': timestamp,
+                        'lat': lat,
+                        'latitudeE7': lat_e7,
+                        'lon': lon,
+                        'longitudeE7': lon_e7,
+                        'pretty': i_date.ctime(),
+                        'url': 'http://maps.google.com/maps?q={lat},{lon}'.format(lat=lat, lon=lon)
+                    })
+
+                    # TODO: Generate KML to check path
+
+                    prev_appended_lat = lat
+                    prev_appended_lon = lon
 
                 prev_lat = lat
                 prev_lon = lon
---
+++ schedule.py
@@ -0,0 +1,16 @@
+import datetime
+
+START = 'start'
+END = 'end'
+
+# SHIFTS = {
+#     2015: {
+#         6: {
+#             30: [
+#                 {
+#                     START:
+#                 }
+#             ]
+#         }
+#     }
+# }
